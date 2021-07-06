133efd655938e144395a5c041763b77277e7b673
wojbiel833 <wojbiel833@gmail.com>
2021-07-06 18:56:12 +0200

    Add new subpages

--- server.js
+++ server.js
@@ -3,24 +3,49 @@ const path = require("path");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname, "/views/index.html");
+  res.show("index.html");
+});
+
+app.get("/home", (req, res) => {
+  res.show("home.html");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(__dirname, "/views/about.html");
+  res.show("about.html");
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile(__dirname, "/views/contact.html");
+  res.show("contact.html");
 });
 
 app.get("/info", (req, res) => {
-  res.sendFile(__dirname, "/views/info.html");
+  res.show("info.html");
 });
 
 app.get("/history", (req, res) => {
-  res.sendFile(__dirname, "/views/history.html");
+  res.show("history.html");
+});
+
+app.get("/user/settings", (req, res) => {
+  res.send("This site is only for users, please log in!");
+});
+
+app.get("/user/panel", (req, res) => {
+  res.send("This site is only for users, please log in!");
+});
+
+app.use((req, res) => {
+  res.status(404).show("404.html");
 });
 
 app.listen(8000, () => {
