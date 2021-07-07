4fd948c074451ab8363f118884a8dd10b982af38
wojbiel833 <wojbiel833@gmail.com>
2021-07-07 17:22:07 +0200

    Add form in contact

--- server.js
+++ server.js
@@ -1,41 +1,59 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("express-handlebars");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine(
+  "hbs",
+  hbs({ extname: "hbs", layoutsDir: "./layouts", defaultLayout: "main" })
+);
+app.set("view engine", "hbs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-  res.show("index.html");
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.show("about.html");
+  res.render("about.hbs", { layout: "dark" });
 });
 
 app.get("/contact", (req, res) => {
-  res.show("contact.html");
+  res.render("contact");
 });
 
 app.get("/info", (req, res) => {
-  res.show("info.html");
+  res.render("info");
 });
 
 app.get("/history", (req, res) => {
-  res.show("history.html");
+  res.render("history");
 });
 
 app.get("/user/settings", (req, res) => {
-  res.show("login.html");
+  res.render("login");
 });
 
 app.get("/user/panel", (req, res) => {
-  res.show("login.html");
+  res.render("login");
+});
+
+app.get("/hello/:name", (req, res) => {
+  res.render("hello", { name: req.params.name });
+});
+
+app.post("/contact/send-message", (req, res) => {
+  const { author, sender, title, message, file } = req.body;
+
+  if (author && sender && title && message && file) {
+    res.render("contact", { isSent: true });
+  } else {
+    res.render("contact", { isError: true });
+  }
 });
 
 app.use((req, res) => {
@@ -45,56 +63,3 @@ app.use((req, res) => {
 app.listen(8000, () => {
   console.log("Server is running on port: 8000");
 });
-
-// const express = require("express");
-// const path = require("path");
-// const hbs = require("express-handlebars");
-
-// const app = express();
-
-// app.engine(
-//   "hbs",
-//   hbs({ extname: "hbs", layoutsDir: "./layouts", defaultLayout: "main" })
-// );
-// app.set("view engine", "hbs");
-
-// app.use(express.static(path.join(__dirname, "/public")));
-
-// app.use((req, res, next) => {
-//   res.show = (name) => {
-//     res.sendFile(path.join(__dirname, `/views/${name}`));
-//   };
-//   next();
-// });
-
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
-
-// app.get("/hello/:name", (req, res) => {
-//   res.render("hello", { name: req.params.name });
-// });
-
-// app.get("/about", (req, res) => {
-//   res.render("about.hbs", { layout: "dark" });
-// });
-
-// app.get("/contact", (req, res) => {
-//   res.render("contact");
-// });
-
-// app.get("/info", (req, res) => {
-//   res.render("info");
-// });
-
-// app.get("/history", (req, res) => {
-//   res.render("history");
-// });
-
-// app.use((req, res) => {
-//   res.status(404).send("404 not found...");
-// });
-
-// app.listen(8000, () => {
-//   console.log("Server is running on port: 8000");
-// });
